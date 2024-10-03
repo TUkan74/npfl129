3da3fab0f002d8041a5b111acf85d342ead7b0d5
Matúš Klečka <klecka.matus@gmail.com>
2024-10-03 21:46:07 +0200

    linear_regression_manual.py

--- labs/01/linear_regression_manual.py
+++ labs/01/linear_regression_manual.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.1, type=lambda x: int(x) if x.isdigit() else float(x), help="Test size")
+parser.add_argument("--test_size", default=0.9, type=lambda x: int(x) if x.isdigit() else float(x), help="Test size")
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 
@@ -18,24 +18,34 @@ def main(args: argparse.Namespace) -> float:
     dataset = sklearn.datasets.load_diabetes()
 
     # The input data are in `dataset.data`, targets are in `dataset.target`.
-
+    
     # If you want to learn about the dataset, you can print some information
     # about it using `print(dataset.DESCR)`.
+    # print(dataset.DESCR)
 
     # TODO: Append a constant feature with value 1 to the end of every input data.
     # Then we do not need to explicitly represent bias - it becomes the last weight.
+    X = np.c_[dataset.data,np.ones(dataset.data.shape[0])]
+    y = dataset.target
 
     # TODO: Split the dataset into a train set and a test set.
     # Use `sklearn.model_selection.train_test_split` method call, passing
     # arguments `test_size=args.test_size, random_state=args.seed`.
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X,y,test_size = args.test_size, random_state = args.seed)
+
 
     # TODO: Solve the linear regression using the algorithm from the lecture,
     # explicitly computing the matrix inverse (using `np.linalg.inv`).
+    XTX_inv = np.linalg.inv(X_train.T @ X_train)
+    w = XTX_inv @ X_train.T @ y_train
 
     # TODO: Predict target values on the test set.
+    y_pred = X_test @ w
 
     # TODO: Manually compute root mean square error on the test set predictions.
-    rmse = ...
+    rmse = np.sqrt(np.mean((y_test - y_pred) **2))
+
 
     return rmse
 
